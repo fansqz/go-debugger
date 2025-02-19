76ecf79f36b41c267c575146d363c9875f1f497f
nbzw <2958556459@qq.com>
2025-02-20 00:19:28 +0800

    feat: 添加BaseRequest

--- protocol/request.go
+++ protocol/request.go
@@ -2,10 +2,27 @@ package protocol
 
 import "go-debugger/constants"
 
-// StartDebugRequest 启动调试请求
-type StartDebugRequest struct {
+type RequestInterface interface {
+	GetSequence() uint
+	SetSequence(uint)
+}
+
+type BaseRequest struct {
 	Type     constants.RequestType `json:"type"`
 	Sequence uint                  `json:"sequence"`
+}
+
+func (b *BaseRequest) GetSequence() uint {
+	return b.Sequence
+}
+
+func (b *BaseRequest) SetSequence(s uint) {
+	b.Sequence = s
+}
+
+// StartDebugRequest 启动调试请求
+type StartDebugRequest struct {
+	BaseRequest
 	// 请求序列号
 	// 用户代码
 	Code string `json:"code"`
@@ -17,69 +34,51 @@ type StartDebugRequest struct {
 
 // AddBreakpointRequest 增加断点请求
 type AddBreakpointRequest struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence    uint  `json:"sequence"`
+	BaseRequest
 	Breakpoints []int `json:"breakpoints"`
 }
 
 // RemoveBreakpointRequest 移除断点请求
 type RemoveBreakpointRequest struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence    uint  `json:"sequence"`
+	BaseRequest
 	Breakpoints []int `json:"breakpoints"`
 }
 
 // SendToConsoleRequest 输入到控制台
 type SendToConsoleRequest struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence uint   `json:"sequence"`
-	Content  string `json:"content"`
+	BaseRequest
+	Content string `json:"content"`
 }
 
 // StepRequest next
 type StepRequest struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence uint               `json:"sequence"`
+	BaseRequest
 	StepType constants.StepType `json:"stepType"`
 }
 
 // ContinueRequest continue
 type ContinueRequest struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence uint `json:"sequence"`
+	BaseRequest
 }
 
 // TerminateRequest 关闭调试
 type TerminateRequest struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence uint `json:"sequence"`
+	BaseRequest
 }
 
 // GetFrameVariables 根据栈帧获取变量列表
 type GetFrameVariables struct {
-	Type    constants.RequestType `json:"type"`
-	FrameID string                `json:"frameID"`
-	// 请求序列号
-	Sequence uint `json:"sequence"`
+	BaseRequest
+	FrameID string `json:"frameID"`
 }
 
 // GetStackTrace 获取栈列表
 type GetStackTrace struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence uint `json:"sequence"`
+	BaseRequest
 }
 
 // GetVariablesRequest 根据引用获取变量信息，如果是指针，获取指针指向的内容，如果是结构体，获取结构体内容
 type GetVariablesRequest struct {
-	Type constants.RequestType `json:"type"`
-	// 请求序列号
-	Sequence  uint                  `json:"sequence"`
+	BaseRequest
 	Reference constants.RequestType `json:"reference"`
 }
