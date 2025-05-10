650910e4f5618ed9ec1bfa93f8c2748e075a4ec5
nbzw <2958556459@qq.com>
2025-05-11 01:52:08 +0800

    feat: 修复bug

--- debugger/gdb_debugger/gdb_debugger.go
+++ debugger/gdb_debugger/gdb_debugger.go
@@ -442,7 +442,7 @@ func (g *GDBDebugger) getLocalVariables(reference int) ([]dap.Variable, error) {
 	// 获取当前线程id
 	currentThreadId, _ := g.getCurrentThreadId()
 	// 获取栈帧中所有局部变量
-	m, err := g.sendWithTimeOut(OptionTimeout, "stack-list-locals",
+	m, err := g.sendWithTimeOut(OptionTimeout, "stack-list-variables",
 		"--thread", currentThreadId, "--frame", strconv.Itoa(frameId), "--skip-unavailable", "2")
 	if err != nil {
 		log.Printf("getLocalScopeVariables failed: %v\n", err)
